a86cb2b1dfa8c22a0628bf2d795976481f66c10f
zmpeg <zandstra.matt@gmail.com>
2015-02-26 15:22:50 -0700

    socket emits data to client on connection

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var values = [
+  { name: 'AAA', value: 12.0 },
+  { name: 'BBB', value: 12.1 },
+  { name: 'CCC', value: 11.9 }
+];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  io.emit('values', values);
 });
 
 http.listen(3000, function(){

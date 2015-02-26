6dd8ca7db28e56d46c56a99ec83cc23927727358
zmpeg <zandstra.matt@gmail.com>
2015-02-26 15:42:03 -0700

    io emits every second new values

--- app.js
+++ app.js
@@ -19,4 +19,10 @@ io.on('connection', function(socket){
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
+  updateValues();
 });
+
+function updateValues(){
+  io.emit('values', values);
+  setTimeout(updateValues, 1000);
+}
